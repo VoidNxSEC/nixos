75c72abf303b7b688f1f40304b983085f7a61d42
VoidNxLabs <sec@voidnxlabs.com>
2026-02-21 12:07:39 -0200

    refactor(kernelcore): add libcanberra-patch-fix.nix for configuration
    This commit refactors the kernelcore configuration by adding a new file libcanberra-patch-fix.nix to handle specific patch requirements for libcanberra. The changes include modifying the existing configuration.nix file and adding new overlays to ensure proper integration and application of the patch.

--- hosts/kernelcore/configuration.nix
+++ hosts/kernelcore/configuration.nix
@@ -938,18 +938,18 @@
   #   ];
   #   config.common.default = "*";
   # };
-  time.timeZone = "America/Scoresbysund";
-  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "America/Bahia";
+  i18n.defaultLocale = "pt_BR.UTF-8";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "kl_GL.UTF-8";
-    LC_IDENTIFICATION = "kl_GL.UTF-8";
-    LC_MEASUREMENT = "kl_GL.UTF-8";
-    LC_MONETARY = "kl_GL.UTF-8";
-    LC_NAME = "kl_GL.UTF-8";
-    LC_NUMERIC = "kl_GL.UTF-8";
-    LC_PAPER = "kl_GL.UTF-8";
-    LC_TELEPHONE = "kl_GL.UTF-8";
-    LC_TIME = "kl_GL.UTF-8";
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
   };
 
   console.keyMap = "br-abnt2";
