6a0c94ad80fb7c54d582610bed1ccdf2f9c34a69
VoidNxLabs <sec@voidnxlabs.com>
2026-06-24 12:38:38 -0300

    build(flake): update flake inputs and documentation
    Update flake.lock with new revisions for dependencies and expand README.md with detailed module distribution table including security, ML, packages, shell, services, and network categories.

--- flake.nix
+++ flake.nix
@@ -70,7 +70,7 @@
 
     # Spider-Nix
     spider-nix = {
-      url = "github:VoidNxSEC/spider-nix";
+      url = "git+ssh://git@github.com/VoidNxSEC/spider-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     i915-governor = {
