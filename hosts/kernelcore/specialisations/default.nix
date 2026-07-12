30a1e6f207a58b02b7422ff1cfd412978d1edb3e
marcosfpina <sec@voidnxlabs.com>
2026-07-12 08:36:20 -0300

    fixes: some fixes

--- hosts/kernelcore/specialisations/default.nix
+++ hosts/kernelcore/specialisations/default.nix
@@ -1,12 +1,12 @@
 {
   imports = [
-    ./k8s-lab.nix
-    ./k8s-prod.nix
-    ./emergency.nix
-    ./cybersecurity.nix
-    ./privacy-paranoia.nix
-    ./development.nix
-    ./stable.nix
+    #./k8s-lab.nix
+    #./k8s-prod.nix
+    #./emergency.nix
+    #./cybersecurity.nix
+    #./privacy-paranoia.nix
+    #./development.nix
+    #./stable.nix
 
     # Niri: aguardando migração do Hyprland
     # ./niri.nix
